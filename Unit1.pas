a4f48ba9abc536dbfa89d0a02e8c455d6a94cab8
admin <n@dam.com>
2016-05-21 20:28:00 +0500

    3

--- Unit1.pas
+++ Unit1.pas
@@ -3,7 +3,8 @@ unit Unit1;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+comobj,  
+Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
 
 
 type
